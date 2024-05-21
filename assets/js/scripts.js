57894f2d4054d9c7cadfd2f14977db95adf41aab
Piotr Zienowicz <115182798+piozien@users.noreply.github.com>
2024-05-21 17:01:41 +0200

    interfejs-1.0

---
+++ assets/js/scripts.js
@@ -0,0 +1,8 @@
+function toggleOrderForm() {
+    var orderForm = document.getElementById("orderForm");
+    if (orderForm.style.display === "none") {
+        orderForm.style.display = "block";
+    } else {
+        orderForm.style.display = "none";
+    }
+}
