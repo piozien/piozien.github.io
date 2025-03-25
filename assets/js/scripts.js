f169e129132c397b813a9bf859751ed19f43d9ce
Piotr Zienowicz <115182798+piozien@users.noreply.github.com>
2025-03-25 15:27:06 +0100

    reformat

--- assets/js/scripts.js
+++
@@ -1,8 +0,0 @@
-function toggleOrderForm() {
-    var orderForm = document.getElementById("orderForm");
-    if (orderForm.style.display === "none") {
-        orderForm.style.display = "block";
-    } else {
-        orderForm.style.display = "none";
-    }
-}
--- dokumentacja/dok2-assets/jss/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('sidebar-toggle').addEventListener('mouseover', function () {
-    document.getElementById('sidebar').style.right = '0';
-});
-
-document.getElementById('sidebar').addEventListener('mouseleave', function () {
-    document.getElementById('sidebar').style.right = '-250px';
-});
